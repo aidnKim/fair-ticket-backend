0fbc4c95f1646422ecbfb0ba2472ac0478e60e59
kimhyeonsoo <hyeonsookim2@gmail.com>
2025-12-06 17:16:02 +0900

    Initial commit

---
+++ src/main/java/com/example/fairticketbackend/FairTicketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fairticketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairTicketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairTicketBackendApplication.class, args);
+	}
+
+}
