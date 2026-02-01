8620d5823f0f37b335d3078109b39a840b828121
HyeonsooKim <hyeonsookim2@gmail.com>
2026-02-01 22:55:26 +0900

    Feat: 웹소켓 구현 완

---
+++ src/main/java/com/fairticket/global/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.fairticket.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");  // 구독 경로
+        config.setApplicationDestinationPrefixes("/app");  // 메시지 전송 경로
+    }
+    
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")  // WebSocket 연결 엔드포인트
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
