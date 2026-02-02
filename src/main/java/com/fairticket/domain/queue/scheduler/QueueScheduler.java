7e0ec607dda4d78084ffda1210b539efc0017a71
HyeonsooKim <hyeonsookim2@gmail.com>
2026-02-02 14:27:45 +0900

    Fix: scheduleId 별로 대기열 적용

--- src/main/java/com/fairticket/domain/queue/scheduler/QueueScheduler.java
+++ src/main/java/com/fairticket/domain/queue/scheduler/QueueScheduler.java
@@ -1,21 +1,33 @@
 package com.fairticket.domain.queue.scheduler;
 
-import com.fairticket.domain.queue.service.QueueService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.fairticket.domain.concert.model.ConcertSchedule;
+import com.fairticket.domain.concert.repository.ConcertScheduleRepository;
+import com.fairticket.domain.queue.service.QueueService;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class QueueScheduler {
 	
     private final QueueService queueService;
+    private final ConcertScheduleRepository scheduleRepository;
     
     @Scheduled(fixedRate = 5000)  // 5초마다
     public void processQueue() {
-        Long scheduleId = 1L;  // 테스트용
-        if (queueService.getQueueSize(scheduleId) > 0) {
-            queueService.allowNextUsers(scheduleId, 10);
+    	// 활성화된 모든 스케줄 조회
+        List<ConcertSchedule> activeSchedules = scheduleRepository.findAll();
+        
+        for (ConcertSchedule schedule : activeSchedules) {
+            Long scheduleId = schedule.getId();
+            if (queueService.getQueueSize(scheduleId) > 0) {
+                queueService.allowNextUsers(scheduleId, 10);
+            }
         }
     }
 }
